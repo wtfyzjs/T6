d090b2676d132033dc3a2da8dafedc601eba4038
wtfyzjs <wtfyzjs@126.com>
2016-05-28 02:08:16 +0800

    ddd

--- src/com/atguigu/crm/Client.java
+++ src/com/atguigu/crm/Client.java
@@ -17,7 +17,7 @@ public class Client
 		System.out.println("1122 add here");
 		System.out.println("1226 add here");
 		System.out.println("233333333333333333333");
-		System.out.println("asdfgj")
+		System.out.println("asdfgggggg");
 	}
 
 }
