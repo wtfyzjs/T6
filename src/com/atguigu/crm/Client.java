e2a2014c74265aafc02cc22de935824e1da7b98c
wtfyzjs <wtfyzjs@126.com>
2016-05-28 01:56:05 +0800

    Update Client.java

--- src/com/atguigu/crm/Client.java
+++ src/com/atguigu/crm/Client.java
@@ -17,6 +17,8 @@ public class Client
 		System.out.println("1122 add here");
 		System.out.println("1226 add here");
 		System.out.println("233333333333333333333");
+		System.out.println("wtfyzjs");
+
 	}
 
 }
